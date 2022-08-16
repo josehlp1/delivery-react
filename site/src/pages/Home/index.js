7528e45bf0c530f0afc0a941e75a8b139d9a2504
José Pellet <52634621+josehlp1@users.noreply.github.com>
2022-08-16 18:47:25 -0300

    Configurações iniciais do projeto

---
+++ site/src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import {ContainerHome} from './style.js';
+
+const Home = (  ) => {
+
+  return (
+    <>
+        <ContainerHome>
+            
+        </ContainerHome>
+    </>
+  );
+}
+
+export default Home;
---
+++ site/src/pages/Home/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ContainerHome = styled.div`
+background-color: red;
+height: 100vh;
+margin: 0px 15%;
+`
